2d0650b556a3654d2c1b77a7a06e17382ca36959
vipulchauhan07 <viratdevo7@gmail.com>
2024-03-17 19:33:53 +0530

    change

--- campaigns/urls/campaign_urls.py
+++ campaigns/urls/campaign_urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from campaigns.views import campaign_views as views
 
 urlpatterns = [
-    path('campaigns/', views.getCampaign, name='get_campaigns'),  # GET endpoint to retrieve campaigns with filters
+    path('', views.getCampaign, name='get_campaigns'),  # GET endpoint to retrieve campaigns with filters
     path('campaigns/<int:campaign_id>/', views.createOrUpdateCampaign, name='create_or_update_campaign'),  # POST and PUT endpoint to create or update a campaign
 ]
